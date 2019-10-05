c0329a3b4c3f78a4d337eceb4bcb08fa6d8310a3
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-10-05 17:57:38 +0200

    WIP

--- src/main.zig
+++ src/main.zig
@@ -26,12 +26,18 @@ pub const WalkDirOptions = struct {
 };
 
 pub const Walker = struct {
+    internal_walker : *DepthFirstWalker,
+
     pub const Self = @This();
 
-    pub fn init(alloc: *std.mem.Allocator) !Self {
+    pub fn init(alloc: *std.mem.Allocator, path: []u8, options: WalkDirOptions) !Self {
+        return Self{
+            .internal_walker = DepthFirstWalker.init(alloc, path),
+        };
     }
 
     pub fn next(self: *Self) !?Entry {
+        return Self.internal_walker.next();
     }
 };
 

427a21e4e8725587838666d8125624f192709bf9
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-11-08 19:14:25 +0100

    Got depth first to working

--- src/depth_first.zig
+++ src/depth_first.zig
@@ -2,28 +2,40 @@ const std = @import("std");
 
 const Entry = @import("entry.zig").Entry;
 
+const PathDirTuple = struct {
+    dir: std.fs.Dir,
+    iter: std.fs.Dir.Iterator,
+    path: []u8,
+};
+
+const RecurseStack = std.atomic.Stack(*PathDirTuple);
+
 pub const DepthFirstWalker = struct {
     startPath    : []u8,
-    recurseStack : std.atomic.Stack(*std.fs.Dir),
+    recurseStack : RecurseStack,
     allocator    : *std.mem.Allocator,
-    maxDepth     : u32,
+    maxDepth     : ?u32,
     hidden       : bool,
 
     currentDir   : std.fs.Dir,
+    currentIter  : std.fs.Dir.Iterator,
     currentPath  : []u8,
     currentDepth : u32,
 
     pub const Self = @This();
 
-    pub fn init(alloc: *std.mem.Allocator, path: []u8, max_depth: u32, include_hidden: bool) !Self {
+    pub fn init(alloc: *std.mem.Allocator, path: []u8, max_depth: ?u32, include_hidden: bool) !Self {
+        var top_dir = try std.fs.Dir.open(path);
+
         return Self{
             .startPath    = path,
-            .recurseStack = std.atomic.Stack(*std.fs.Dir).init(),
+            .recurseStack = RecurseStack.init(),
             .allocator    = alloc,
             .maxDepth     = max_depth,
             .hidden       = include_hidden,
 
-            .currentDir   = try std.fs.Dir.open(path),
+            .currentDir   = top_dir,
+            .currentIter  = top_dir.iterate(),
             .currentPath  = path,
             .currentDepth = 0,
         };
@@ -31,27 +43,42 @@ pub const DepthFirstWalker = struct {
 
     pub fn next(self: *Self) !?Entry {
         outer: while (true) {
-            if (try self.currentDir.next()) |entry| {
+            if (try self.currentIter.next()) |entry| {
                 const full_entry_path = try self.allocator.alloc(u8, self.currentPath.len + entry.name.len + 1);
                 std.mem.copy(u8, full_entry_path, self.currentPath);
                 full_entry_path[self.currentPath.len] = std.fs.path.sep;
                 std.mem.copy(u8, full_entry_path[self.currentPath.len + 1 ..], entry.name);
     
-                if (entry.kind == std.fs.Dir.Entry.Kind.Directory) {
-                    if (self.currentDepth < self.maxDepth) {
-                        const new_dir = try self.allocator.create(std.atomic.Stack(*std.fs.Dir).Node);
-                        new_dir.* = std.atomic.Stack(*std.fs.Dir).Node {
-                            .next = undefined,
-                            .data = &self.currentDir,
+                blk: {
+                    if (entry.kind == std.fs.Dir.Entry.Kind.Directory) {
+                        if (self.maxDepth) |max_depth| {
+                            if (self.currentDepth >= max_depth) {
+                                break :blk;
+                            }
+                        }
+
+                        const new = try self.allocator.create(PathDirTuple);
+                        new.* = PathDirTuple {
+                            .dir = self.currentDir,
+                            .iter = self.currentIter,
+                            .path = self.currentPath,
                         };
-    
+
+                        const new_dir = try self.allocator.create(RecurseStack.Node);
+                        new_dir.* = RecurseStack.Node {
+                            .next = undefined,
+                            .data = new,
+                        };
+
                         // Save the current opened directory to the stack
                         // so we continue traversing it later on
                         self.recurseStack.push(new_dir);
-    
+
                         // Go one level deeper
-                        const opened_dir = try std.fs.Dir.open(full_entry_path);
+                        var opened_dir = try std.fs.Dir.open(full_entry_path);
+                        self.currentPath = full_entry_path;
                         self.currentDir = opened_dir;
+                        self.currentIter = opened_dir.iterate();
                         self.currentDepth += 1;
                     }
                 }
@@ -66,11 +93,14 @@ pub const DepthFirstWalker = struct {
                 // No entries left in the current dir
                 self.currentDir.close();
                 if (self.recurseStack.pop()) |node| {
-
                     // Go back up one level again
-                    self.currentDir = node.data.*;
-                    self.allocator.destroy(node);
+                    self.currentDir = node.data.dir;
+                    self.currentIter = node.data.iter;
+                    self.currentPath = node.data.path;
                     self.currentDepth -= 1;
+
+                    self.allocator.destroy(node.data);
+                    self.allocator.destroy(node);
 
                     continue :outer;
                 }

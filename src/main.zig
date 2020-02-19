daf4541739c0a16196f1a5a344832db173c15ddb
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-02-19 14:54:10 +0100

    Fix

---
+++ src/main.zig
@@ -0,0 +1,7 @@
+const breadth_first = @import("breadth_first.zig");
+const depth_first = @import("depth_first.zig");
+const entry = @import("entry.zig");
+
+pub const BreadthFirstWalker = breadth_first.BreadthFirstWalker;
+pub const DepthFirstWalker = depth_first.DepthFirstWalker;
+pub const Entry = entry.Entry;
